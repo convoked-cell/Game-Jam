73675fbd9451f2d661828941481f72079379aa33
Julien <juleus6@gmail.com>
2021-02-07 13:17:17 -0500

    First Commit
    Import of Unity Game

---
+++ Game/Assets/Japanese School Modular Pack/Scripts/Door.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public bool IsOpenOnStart; // Do not use this to toggle the door while in play. Use ToggleDoor function instead
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("IsOpen", IsOpenOnStart);
+    }
+
+    public void ToggleDoor() // Call this function to toggle the door!
+    {
+        IsOpenOnStart = !IsOpenOnStart;
+        anim.SetBool("IsOpen", IsOpenOnStart);
+    }
+}
---
+++ Game/Assets/Japanese School Modular Pack/Scripts/PlayerOpenDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOpenDoor : MonoBehaviour
+{
+
+    public float rayDistance = 5f; // distance to check for door collider
+    public LayerMask playerLayer;
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // shoots a ray from camera 
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            if (hit.collider.CompareTag("Door")) // checks if the ray hit a collider tagged as "Door"
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    hit.collider.gameObject.GetComponent<Door>().ToggleDoor(); // if player object presses "E", the door's "ToggleDoor" function gets called
+                }
+            }
+        }
+    }
+}
